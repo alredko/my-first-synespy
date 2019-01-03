3196431d62664fa9c57efa5fa383387d9b6ca630
Oleksii <redkoal@gmail.com>
2019-01-03 11:10:07 +0200

    Пути к файлам и вид 5 карточек

--- cards/models.py
+++ cards/models.py
@@ -5,15 +5,15 @@ class Word(models.Model):
     num = models.PositiveIntegerField("№ пп")
     word = models.CharField("Слово", max_length=20)
     imagew = models.ImageField("Образ слова", upload_to='cards/')
-    soundw = models.FileField("Звучание слова", upload_to='files/')
+    soundw = models.FilePathField("Звучание слова", path="files/")
     transcr = models.CharField("Транскрипция", max_length=20)
     imaget = models.ImageField("Образ звучания", upload_to='cards/')
     association = models.CharField("Ассоциация", max_length=20)
     interpret = models.CharField("Перевод", max_length=20)
     example1 = models.CharField("Пример 1", max_length=200)
-    sounde1 = models.FileField("Озвучка Пр1", upload_to='files/')
+    sounde1 = models.FilePathField("Озвучка Пр1", path="files/")
     example2 = models.CharField("Пример 2", max_length=200)
-    sounde2 = models.FileField("Озвучка Пр2", upload_to='files/')
+    sounde2 = models.FilePathField("Озвучка Пр2", path="files/")
     explain = models.TextField("Пояснение")
 
     def __str__(self):
--- users/models.py
+++ users/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    ('NEW', 'New'),
+    ('TOURIST', 'Tourist'),
+    ('IMMIGRANT', 'Immigrant'),
+    ('ABORIGINE', 'Aborigine'),
+)
 
 class User(models.Model):
-    
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    status = models.CharField(max_length=1)
-    progress = models.CharField(max_length=10)
-    created_date = models.DateTimeField('date created')
 
-
+    name = models.CharField("Имя", max_length=50)
+    email = models.EmailField("E-Mail", max_length=50)
+    status = models.CharField("Статус", max_length=10)
+    progress = models.CharField("Порядок карточек", max_length=10)
+    created_date = models.DateTimeField('Время регистрации')
 
     def __str__(self):
         return self.name
